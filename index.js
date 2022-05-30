30b1478b176b2c59c1b60dcaf399b005ab7feb79
algonacci <algonacci1@gmail.com>
2022-05-30 10:33:12 +0700

    feat: Demo Fulfillment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const bodyParser = require("body-parser");
+const { WebhookClient, Payload } = require("dialogflow-fulfillment");
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+const app = express();
+app.use(bodyParser.json());
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.post("/", express.json(), (request, response) => {
+  const agent = new WebhookClient({ request, response });
+
+  function demo(agent) {
+    agent.add("Sending response from Webhook server as Bilip v.3.0.0!");
+  }
+
+  var intentMap = new Map();
+  
+  intentMap.set("Demo", demo);
+
+  agent.handleRequest(intentMap);
 });
