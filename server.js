cb8d0deca000871f2a3cfc8edc01443da225c45f
algonacci <algonacci1@gmail.com>
2022-05-30 15:55:37 +0700

    demo

--- server.js
+++ server.js
@@ -21,9 +21,62 @@ app.post("/", express.json(), (request, response) => {
     agent.add("Sending response from Webhook server as Bilip v.3.0.0!");
   }
 
+  function arrangeMeeting(agent) {
+    var payloadData = {
+      richContent: [
+        [
+          {
+            type: "info",
+            title: "Available Dates",
+            subtitle:
+              "Your Academic Director is Available on these dates. Please choose one:",
+          },
+          {
+            type: "divider",
+          },
+          {
+            type: "list",
+            title: "31/05/2022",
+            event: {
+              name: "",
+              languageCode: "",
+              parameters: {},
+            },
+          },
+          {
+            type: "list",
+            title: "02/06/2022",
+            event: {
+              name: "",
+              languageCode: "",
+              parameters: {},
+            },
+          },
+          {
+            type: "list",
+            title: "03/06/2022",
+            event: {
+              name: "",
+              languageCode: "",
+              parameters: {},
+            },
+          },
+        ],
+      ],
+    };
+
+    agent.add(
+      new Payload(agent.UNSPECIFIED, payloadData, {
+        sendAsMessage: true,
+        rawPayload: true,
+      })
+    );
+  }
+
   var intentMap = new Map();
-  
+
   intentMap.set("Demo", demo);
+  intentMap.set("A06_ArrangeMeeting", arrangeMeeting);
 
   agent.handleRequest(intentMap);
 });
