6763986e80210f3143e99839eb4867763120ef66
algonacci <algonacci1@gmail.com>
2022-05-31 11:20:51 +0700

    feat: Get meeting date

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.post("/", express.json(), (request, response) => {
   }
 
   function arrangeMeeting(agent) {
+    const a = 1;
+    const b = 2;
+    const c = 3;
     var payloadData = {
       richContent: [
         [
@@ -38,27 +41,33 @@ app.post("/", express.json(), (request, response) => {
             type: "list",
             title: "31/05/2022",
             event: {
-              name: "",
+              name: "meeting_date",
               languageCode: "",
-              parameters: {},
+              parameters: {
+                value: a,
+              },
             },
           },
           {
             type: "list",
             title: "02/06/2022",
             event: {
-              name: "",
+              name: "meeting_date",
               languageCode: "",
-              parameters: {},
+              parameters: {
+                value: b,
+              },
             },
           },
           {
             type: "list",
             title: "03/06/2022",
             event: {
-              name: "",
+              name: "meeting_date",
               languageCode: "",
-              parameters: {},
+              parameters: {
+                value: c,
+              },
             },
           },
         ],
@@ -73,10 +82,157 @@ app.post("/", express.json(), (request, response) => {
     );
   }
 
+  function getMeetingDate(agent) {
+    agent.add("Alright, you already choose this selected date:");
+    console.log(request.body.queryResult.outputContexts);
+
+    infoContext = agent.context.get("meeting_date");
+    console.log(infoContext);
+    choice = infoContext.parameters.value;
+
+    if (choice == 1) {
+      var payloadData = {
+        richContent: [
+          [
+            {
+              type: "info",
+              title: "Final Meeting Confirmation",
+              subtitle:
+                "Please confirm are you sure want to arrange a meeting with the Academic Director?",
+            },
+            {
+              type: "divider",
+            },
+            {
+              type: "chips",
+              options: [
+                {
+                  image: {
+                    src: {
+                      rawUrl:
+                        "https://raw.githubusercontent.com/algonacci/Free-CDN/main/succes_icon.png",
+                    },
+                  },
+                  text: "Yes, I'm sure",
+                },
+                {
+                  image: {
+                    src: {
+                      rawUrl:
+                        "https://raw.githubusercontent.com/algonacci/Free-CDN/main/fail_icon.png",
+                    },
+                  },
+                  text: "No, I want to reschedule",
+                },
+              ],
+            },
+          ],
+        ],
+      };
+      agent.add(
+        new Payload(agent.UNSPECIFIED, payloadData, {
+          sendAsMessage: true,
+          rawPayload: true,
+        })
+      );
+    } else if (choose == 2) {
+      var payloadData = {
+        richContent: [
+          [
+            {
+              type: "info",
+              title: "Final Meeting Confirmation",
+              subtitle:
+                "Please confirm are you sure want to arrange a meeting with the Academic Director?",
+            },
+            {
+              type: "divider",
+            },
+            {
+              type: "chips",
+              options: [
+                {
+                  image: {
+                    src: {
+                      rawUrl:
+                        "https://raw.githubusercontent.com/algonacci/Free-CDN/main/succes_icon.png",
+                    },
+                  },
+                  text: "Yes, I'm sure",
+                },
+                {
+                  image: {
+                    src: {
+                      rawUrl:
+                        "https://raw.githubusercontent.com/algonacci/Free-CDN/main/fail_icon.png",
+                    },
+                  },
+                  text: "No, I want to reschedule",
+                },
+              ],
+            },
+          ],
+        ],
+      };
+      agent.add(
+        new Payload(agent.UNSPECIFIED, payloadData, {
+          sendAsMessage: true,
+          rawPayload: true,
+        })
+      );
+    } else {
+      var payloadData = {
+        richContent: [
+          [
+            {
+              type: "info",
+              title: "Final Meeting Confirmation",
+              subtitle:
+                "Please confirm are you sure want to arrange a meeting with the Academic Director?",
+            },
+            {
+              type: "divider",
+            },
+            {
+              type: "chips",
+              options: [
+                {
+                  image: {
+                    src: {
+                      rawUrl:
+                        "https://raw.githubusercontent.com/algonacci/Free-CDN/main/succes_icon.png",
+                    },
+                  },
+                  text: "Yes, I'm sure",
+                },
+                {
+                  image: {
+                    src: {
+                      rawUrl:
+                        "https://raw.githubusercontent.com/algonacci/Free-CDN/main/fail_icon.png",
+                    },
+                  },
+                  text: "No, I want to reschedule",
+                },
+              ],
+            },
+          ],
+        ],
+      };
+      agent.add(
+        new Payload(agent.UNSPECIFIED, payloadData, {
+          sendAsMessage: true,
+          rawPayload: true,
+        })
+      );
+    }
+  }
+
   var intentMap = new Map();
 
   intentMap.set("Demo", demo);
   intentMap.set("A06_ArrangeMeeting", arrangeMeeting);
+  intentMap.set("A06_ArrangeMeeting - custom", getMeetingDate);
 
   agent.handleRequest(intentMap);
 });
